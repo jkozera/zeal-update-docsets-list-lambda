75cce94ef3fd87ddc66f8212b0565af5ec03fa95
Jerzy Kozera <jerzy.kozera@gmail.com>
2018-09-16 15:54:39 +0200

    Push docsets to zealdocs.github.io too

--- update_docsets/__init__.py
+++ update_docsets/__init__.py
@@ -29,7 +29,7 @@ def get_dash_usercontributed_docsets():
         res.append('- title: %s' % doc['name'])
         res.append('  icon: %s' % doc.get('icon', doc.get('icon@2x', '""')))
 
-    return '\n'.join(res)
+    return '\n'.join(res) + '\n'
 
 
 def get_dash_docsets():
@@ -43,11 +43,11 @@ def get_dash_docsets():
         res.append('- title: %s' % doc['title'])
         res.append('  icon: %s' % doc['icon'])
 
-    return '\n'.join(res)
+    return '\n'.join(res) + '\n'
 
 
 TARGET = '/tmp/zevdocs.io'
-REMOTE = 'ssh://git@github.com/jkozera/zevdocs.io'
+REMOTE = 'ssh://git@github.com/%s'
 
 
 def data_uri(data):
@@ -62,7 +62,14 @@ def get_daily_downloads(day):
     ).json()["refs"]["io.github.jkozera.ZevDocs"].values()])
 
 
-def main(json_input=None, context=None):
+def process_repo(remote,
+                 with_usercontrib=True,
+                 with_downloads=True,
+                 commit_message=None,
+                 target_branch='gh-pages',
+                 cache=None):
+    cache = cache or {}
+
     try:
         shutil.rmtree(TARGET)
     except FileNotFoundError:
@@ -70,11 +77,12 @@ def main(json_input=None, context=None):
 
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('deploy-keys')
-    key = table.get_item(Key={'repo_name': 'jkozera/zevdocs.io'})['Item']['key']
+    key = table.get_item(Key={'repo_name': remote})['Item']['key']
     with open('/tmp/keyfile', 'w') as keyfile:
         keyfile.write(key)
+
     repo = porcelain.clone(
-        REMOTE, TARGET,
+        REMOTE % remote, TARGET,
         key_filename='/tmp/keyfile'
     )
 
@@ -83,46 +91,57 @@ def main(json_input=None, context=None):
     except FileExistsError:
         pass
 
-    docsets = get_dash_docsets()
+    paths = ['_data/docsets.yml']
+    cache['docsets'] = cache.get('docsets') or get_dash_docsets()
     with open('%s/_data/docsets.yml' % TARGET, 'w') as f:
-        f.write(docsets)
+        f.write(cache['docsets'])
 
-    usercontributed_docsets = get_dash_usercontributed_docsets()
-    with open('%s/_data/docsets_usercontributed.yml' % TARGET, 'w') as f:
-        f.write(usercontributed_docsets)
+    if with_usercontrib:
+        usercontributed_docsets = get_dash_usercontributed_docsets()
+        with open('%s/_data/docsets_usercontributed.yml' % TARGET, 'w') as f:
+            f.write(usercontributed_docsets)
+        paths += ['_data/docsets_usercontributed.yml']
 
-    with open('%s/_data/weekly_downloads.yml' % TARGET, 'w') as f:
-        today = datetime.date.today()
-        weekly_downloads = [
-            get_daily_downloads(today - datetime.timedelta(days=days))
-            for days in range(7, 0, -1)
-        ]
-        sparkline = data_uri(sparklines.impulse(
-            weekly_downloads,
-            below_color='yellow',
-            above_color='MediumSpringGreen',
-            width=3,
-            dmin=0,
-            dmax=max(weekly_downloads)
-        ))
-        f.write('count: %s\n' % sum(weekly_downloads))
-        f.write('sparkline: %s\n' % sparkline)
+    if with_downloads:
+        with open('%s/_data/weekly_downloads.yml' % TARGET, 'w') as f:
+            today = datetime.date.today()
+            weekly_downloads = [
+                get_daily_downloads(today - datetime.timedelta(days=days))
+                for days in range(7, 0, -1)
+            ]
+            sparkline = data_uri(sparklines.impulse(
+                weekly_downloads,
+                below_color='yellow',
+                above_color='MediumSpringGreen',
+                width=3,
+                dmin=0,
+                dmax=max(weekly_downloads)
+            ))
+            f.write('count: %s\n' % sum(weekly_downloads))
+            f.write('sparkline: %s\n' % sparkline)
+        paths += ['_data/weekly_downloads.yml']
 
     os.chdir(TARGET)
-    porcelain.add(paths=['_data/docsets.yml',
-                         '_data/docsets_usercontributed.yml',
-                         '_data/weekly_downloads.yml'])
+    porcelain.add(paths=paths)
     jk = 'Jerzy Kozera <jerzy.kozera@gmail.com>'
     porcelain.commit(
-        message="Update docset list and weekly downloads %s" % datetime.datetime.now().isoformat(),
+        message=commit_message or ("Update docset list and weekly downloads %s" % datetime.datetime.now().isoformat()),
         author=jk,
         committer=jk
     )
     commits = [e.commit.tree for e in repo.get_walker(max_entries=2)][:2]
     if sum(1 for _ in repo.object_store.tree_changes(commits[1], commits[0])):
-        porcelain.push('.', REMOTE, [b'master:gh-pages'],
+        porcelain.push('.', REMOTE % remote, [b'master:%s' % target_branch.encode('ascii')],
                        key_filename='/tmp/keyfile')
 
+def main(json_input=None, context=None):
+    cache = process_repo('jkozera/zevdocs.io')
+    process_repo('zealdocs/zealdocs.github.io',
+                 with_usercontrib=False,
+                 with_downloads=False,
+                 commit_message='chore(data): update docset list',
+                 target_branch='master',
+                 cache=cache)
 
 if __name__ == '__main__':
     main()
